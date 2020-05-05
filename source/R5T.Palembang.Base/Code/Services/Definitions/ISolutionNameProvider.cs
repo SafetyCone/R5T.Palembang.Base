1d440d5caedbc8712564211fa41d659cc3f053ff
MinexAutomation <minex.automation@gmail.com>
2020-05-04 18:26:52 -0700

    Added IProjectNameProvider and ISolutionNameProject service definitions.

---
+++ source/R5T.Palembang.Base/Code/Services/Definitions/ISolutionNameProvider.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+
+namespace R5T.Palembang
+{
+    /// <summary>
+    /// Provides the project name (note, NOT the project file name).
+    /// </summary>
+    public interface ISolutionNameProvider
+    {
+        string GetSolutionName();
+    }
+}
