3a6536c088f536e00a856e406b063354c699621c
davidcoats <david@getsafetycone.com>
2023-02-10 17:49:47 -0800

    Interim changes.

--- source/R5T.Palembang.Base/Code/Services/Definitions/IBuildConfigurationNameProvider.cs
+++ source/R5T.Palembang.Base/Code/Services/Definitions/IBuildConfigurationNameProvider.cs
@@ -2,10 +2,11 @@ using System;using R5T.T0064;
 
 
 namespace R5T.Palembang
-{[ServiceDefinitionMarker]
+{
     /// <summary>
     /// Provides the build configuration name (examples: "Release", "Debug").
     /// </summary>
+    [ServiceDefinitionMarker]
     public interface IBuildConfigurationNameProvider:IServiceDefinition
     {
         string GetBuildConfigurationName();
--- source/R5T.Palembang.Base/Code/Services/Definitions/ISolutionNameProvider.cs
+++ source/R5T.Palembang.Base/Code/Services/Definitions/ISolutionNameProvider.cs
@@ -2,10 +2,11 @@ using System;using R5T.T0064;
 
 
 namespace R5T.Palembang
-{[ServiceDefinitionMarker]
+{
     /// <summary>
     /// Provides the project name (note, NOT the project file name).
     /// </summary>
+    [ServiceDefinitionMarker]
     public interface ISolutionNameProvider:IServiceDefinition
     {
         string GetSolutionName();
